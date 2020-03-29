da8942921f7ae11b9140b6b3bbd9d1d2bf50caf1
xjjj2 <xiejie1006@sjtu.edu.cn>
2020-03-29 20:20:04 +0800

    deletion

--- acmdb-lab1/databuilder.py
+++
@@ -1,8 +0,0 @@
-file=open("some_data_file.txt",'w')
-for i in range(0,100):
-    for j in range(0,100):
-        file.write(str(i)+"0"+str(j))
-        if (j!=99):file.write(",")
-    file.write('\r\n')
-file.close()
-        
